bb3bac3fd4e29eef2aa41b4114a56303b91b0b08
Nathan Garcia <nategarcia.va@gmail.com>
2022-12-06 10:58:06 -0500

    "Fixed indents"

--- compare_users.ps1
+++ compare_users.ps1
@@ -3,29 +3,29 @@ Import-Module ActiveDirectory
 
 function Compare-UserObjectProperties{
     Param(
-    	$identity1,
-    	$identity2
+        $identity1,
+        $identity2
     )
 
-	Begin{
-		$userclass=[adsi]"LDAP://schema/user"
-		$allproperties=$userclass.mandatoryproperties
-		$allproperties+=$userclass.optionalproperties
-	}
-	Process{
-		Try{
-			$user1=Get-Aduser $identity1 -Properties * -ea Stop
-			$user2=Get-Aduser $identity2 -Properties * -ea Stop
-			foreach($p in $allproperties){
-			   [pscustomobject]@{
-			           PropertyName=$p
-			           User1=$user1.psobject.Properties[$p].Value
-			           User2=$user2.psobject.Properties[$p].Value
-			    }
-			}
-		}
-		Catch{Throw $_}
-	}
+    Begin{
+        $userclass=[adsi]"LDAP://schema/user"
+        $allproperties=$userclass.mandatoryproperties
+        $allproperties+=$userclass.optionalproperties
+    }
+    Process{
+        Try{
+            $user1=Get-Aduser $identity1 -Properties * -ea Stop
+            $user2=Get-Aduser $identity2 -Properties * -ea Stop
+            foreach($p in $allproperties){
+               [pscustomobject]@{
+                       PropertyName=$p
+                       User1=$user1.psobject.Properties[$p].Value
+                       User2=$user2.psobject.Properties[$p].Value
+                }
+            }
+        }
+        Catch{Throw $_}
+    }
 }
 
 # Compare-UserObjectProperties User1 User2 | Out-GridView
